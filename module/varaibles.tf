7f191c45704f0ef3cfef7a6998e737cbde3deee0
ouatedephoque1107 <florent.debove@epsi.fr>
2021-05-05 15:59:21 +0200

    2

---
+++ module/varaibles.tf
@@ -0,0 +1,32 @@
+variable "reponame" {
+  description = "reponame"
+  type = string
+}
+
+variable "repoprive" {
+  description = "repoprive"
+  default = true
+  type = bool
+}
+
+variable "repoignore" {
+  description = "repoignore"
+  type = string
+}
+
+variable "repowiki" {
+  description = "repowiki"
+  default = false
+  type = bool
+}
+
+variable "membername" {
+  description = "membername"
+  type = string
+}
+
+variable "teamname" {
+  description = "teamname"
+  type = string
+}
+
